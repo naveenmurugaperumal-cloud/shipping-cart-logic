15d9661e700c4cac3fd2c55c9b5e4f71744314a6
naveenmurugaperumal-cloud <naveen.murugaperumal@gmail.com>
2026-07-07 00:06:06 +0530

    remove unwanted content in shipping

--- shipping.py
+++ shipping.py
@@ -1,22 +1,18 @@
-# Shipping Cart Logic
 
-# Get input from user
 price = float(input("Enter the price of the product: "))
 discount = float(input("Enter the discount percentage: "))
 gst = float(input("Enter the GST percentage: "))
 
-# Calculate discount amount
+
 discount_amount = (price * discount) / 100
 
-# Price after discount
+
 price_after_discount = price - discount_amount
 
-# Calculate GST
+
 gst_amount = (price_after_discount * gst) / 100
 
-# Final amount
+
 total_amount = price_after_discount + gst_amount
 
-# Display results
-print("\n------ Bill Details ------")
 print("Original Price      :", price)
